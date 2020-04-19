ba14daf6275c25385b4318b1f1429db380e6c542
dani <daniehaid@tsn.at>
2020-04-19 15:43:59 +0200

    Funktioniert jetzt :)

--- recursive/BinarySearch.java
+++ recursive/BinarySearch.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class BinarySearch {
 	
 	public static int binarySearch(int gesuchteZahl, List<Integer> sortierteZahlenliste)	{
-		int index = sortierteZahlenliste.size()/2 - 1;
+		int index = sortierteZahlenliste.size() / 2;
 		if(gesuchteZahl == sortierteZahlenliste.get(index))	{
 			return index;
 		}
@@ -30,16 +30,17 @@ public class BinarySearch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> liste = new ArrayList<>();
-//		liste.add(2);
-		liste.add(2); //funktioniert
-		liste.add(3); //funktioniert nicht
-		liste.add(4); //funktioniert
-//		liste.add(4);
-//		liste.add(4);
-		liste.add(5); //funktioniert
-		liste.add(6); //funktioniert nicht
-		liste.add(7); //funktioniert nicht
-		System.out.println(binarySearch(7, liste));
+		liste.add(2);
+		liste.add(2); 
+		liste.add(3); 
+		liste.add(4); 
+		liste.add(4);
+		liste.add(4);
+		liste.add(5); 
+		liste.add(6); 
+		liste.add(7); 
+		liste.add(8);
+		System.out.println(binarySearch(8, liste));
 	}
 
 }
